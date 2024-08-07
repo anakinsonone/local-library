b698a1abe6822328ae150f8154840a90becdcd06
anakinsonone <akkhi.sonone@gmail.com>
2024-08-07 23:31:24 +0530

    add login, signup and logout links to home page

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -28,6 +28,7 @@ exports.index = asyncHandler(async (req, res, next) => {
     book_instance_available_count: numAvailableBookInstances,
     author_count: numAuthors,
     genre_count: numGenres,
+    isLoggedIn: req.session && req.session.user,
   });
 });
 
